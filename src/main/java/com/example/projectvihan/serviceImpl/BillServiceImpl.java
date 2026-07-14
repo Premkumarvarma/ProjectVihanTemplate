995229ac2c23f9bcace8753d2a454430af79a294
premKumar <bodakuntaprem2025@gmail.com>
2026-07-14 11:09:33 +0530

    Bill service impl business logic

--- src/main/java/com/example/projectvihan/serviceImpl/BillServiceImpl.java
+++ src/main/java/com/example/projectvihan/serviceImpl/BillServiceImpl.java
@@ -49,20 +49,55 @@ private static Logger logger = LoggerFactory.getLogger(BillServiceImpl.class);
 
 	@Override
 	public Bill deleteBillById(Long id) throws AppServiceException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Start of Bill delete method>>>>>>>>>>" + id);
+		try {
+			Bill billObj = billRepository.findById(id).orElse(null);
+			if(billObj != null) {
+				billRepository.deleteById(id);
+				logger.info("Bill  deleted successfully with id");
+			}
+			logger.info("End of deleteBillId service method <<<");
+			return billObj;
+			
+		}catch (Exception e) {
+			logger.error("Error while deleting Bill", e);
+			throw new AppServiceException("DB_ERROR","Unable to delete bill",e);
+		}
 	}
 
 	@Override
 	public Bill findBillById(Long id) throws AppServiceException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Start of Bill delete method>>>>>>>>>>" + id);
+		try {
+			Bill billObj = billRepository.findById(id).orElse(null);
+			if(billObj != null) {
+				billRepository.findById(id);
+				logger.info("Bill details find successfully with given id");
+			} else {
+				logger.info("Unable to Find Bill details with given id");
+			}
+			logger.info("End of findbyid service method <<<");
+			return billObj;
+			
+		}catch (Exception e) {
+			logger.error("Error while finding the  Bill", e);
+			throw new AppServiceException("DB_ERROR","Unable to find bill",e);
+		}
 	}
 
 	@Override
 	public List<Bill> findAllBill() throws AppServiceException {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Start of Bill find all method>>>>>>>>>>");
+		List<Bill> billObj = null;
+		try {
+			billObj = billRepository.findAll();
+			logger.info("successfully fetch the billlist");
+			
+		}catch (Exception e) {
+			logger.error("Error while finding the  Bill", e);
+			throw new AppServiceException("DB_ERROR","Unable to find bill",e);
+		}
+		return billObj;	
 	}
 
 }
